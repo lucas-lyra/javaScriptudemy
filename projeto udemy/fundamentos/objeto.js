a1cde0193fe308898ab7fa46a2543353633b0888
lucas-lyra <lucas.slyra99@hotmail.com>
2023-07-25 10:53:34 -0300

    add

--- projeto udemy/fundamentos/objeto.js
+++ projeto udemy/fundamentos/objeto.js
@@ -4,3 +4,10 @@ prod1.preço = 4998.90
 prod1['Desconto Legal']= 0.40 //evitar atributos com espaço 
 
 console.log(prod1);
+
+const prod2 = {
+    nome: 'camisa polo',
+    preço: 79.90
+}
+
+console.log(prod2);
