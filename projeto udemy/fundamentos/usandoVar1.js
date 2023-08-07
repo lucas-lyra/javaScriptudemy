11492462c6dc49af498987ff062cc4f6595035aa
lucas-lyra <lucas.slyra99@hotmail.com>
2023-08-07 08:57:51 -0300

    att 2

---
+++ projeto udemy/fundamentos/usandoVar1.js
@@ -0,0 +1,19 @@
+
+{
+    {
+        {
+            {
+                var sera = 'será??'
+            }
+        }
+    }
+}
+
+console.log(sera);
+
+function teste(){
+    var local = 123
+
+}
+teste()
+console.log(local);
