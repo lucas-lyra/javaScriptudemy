864f49ee453aebf8da6b390ada54590523179ad3
lucas-lyra <121949176+lucas-lyra@users.noreply.github.com>
2023-09-11 18:15:40 -0300

    Create if2.js

---
+++ projeto udemy/Controle/if2.js
@@ -0,0 +1,19 @@
+function teste1(num){
+    if(num > 7)
+    console.log(num);
+
+console.log('final');
+
+}
+
+teste1(6)
+teste1(8)
+
+function teste2 ( num ) {
+    if( num > 7);{ // cuidado !!! = ;
+        console.log(num);
+    }
+}
+teste2(6)
+teste2(8)
+//nao use ponto e virgula com as estruturas de controle 
